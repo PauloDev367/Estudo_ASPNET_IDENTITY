7f244c6a9745f08b1e0583afe70a68e57868e092
Paulo <pauloecomercial367@gmail.com>
2024-06-18 20:06:00 -0300

    :sparkles: Configurações básicas para o identity

---
+++ EstudoIdentity/Data/IdentityDataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoIdentity.Data;
+
+public class IdentityDataContext : IdentityDbContext
+{
+    public IdentityDataContext(DbContextOptions<IdentityDataContext> options) : base(options) { }
+}
--- EstudoIdentity/Program.cs
+++ EstudoIdentity/Program.cs
@@ -1,15 +1,22 @@
+using EstudoIdentity.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connString = "Server=localhost,1433;Database=EstudoIdentity;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+builder.Services.AddDbContext<IdentityDataContext>(opt =>
+{
+    opt.UseSqlServer(connString);
+});
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
